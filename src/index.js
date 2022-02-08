33761f48ad89cb6012dd9789d86343b1e0149031
cathome1 <31789194+cathome1@users.noreply.github.com>
2022-02-08 12:14:40 +0300

    feat: test build

--- src/index.js
+++ src/index.js
@@ -38,7 +38,24 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  arr1 = expr.split('**********')
+  words = ''
+  for (i in arr1){
+    word = []
+    nowArr = arr1[i].split('')
+    while(nowArr.length>0){
+      nowLetter = ''
+      letter = nowArr.splice(0,10)
+      while (letter[0]=='0') letter.shift()
+      while (letter.length>0){
+        sign = letter.splice(0,2).join('')
+        if (sign==='10') nowLetter+='.'
+        else nowLetter+='-'}
+        word.push(MORSE_TABLE[nowLetter])
+    }
+    words+=word.join('')+' '
+  }
+  return words
 }
 
 module.exports = {
